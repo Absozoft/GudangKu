bf309b79d3b405d2dbea970d998f118e1534703e
cinta amelia putri <ameliacinta135@gmail.com>
2025-11-26 17:00:37 +0700

    wdaw

--- Gudang/server.js
+++ Gudang/server.js
@@ -12,6 +12,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+//untuk menampilkan file di folder public
+app.use(express.static('public'));
+
 // Middleware untuk parsing JSON dari request body
 app.use(express.json());
 
